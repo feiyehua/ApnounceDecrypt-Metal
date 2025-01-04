04dd5c7e13dd8ea68de453ae707f90f857a4d6cb
FeiYehua <31472675+feiyehua@users.noreply.github.com>
2025-01-04 08:49:14 +0800

    Initial Commit

---
+++ ApnounceDecrypt-Metal/main.cpp
@@ -0,0 +1,21 @@
+//
+    /*******************************************************************************
+            
+            File name:     main.cpp
+            Author:        FeiYehua
+            
+            Description:   Created for ApnounceDecrypt-Metal in 2025
+            
+            History:
+                    2025/1/4: File created.
+            
+    ********************************************************************************/
+    
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
